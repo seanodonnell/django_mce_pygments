3b77758557405104070b15fe081c274df3e3eff9
Sean O'Donnell <sean@odonnell.nu>
2013-11-21 01:28:06 +0000

    pep8 cleanup

--- src/django_mce_pygments/urls.py
+++ src/django_mce_pygments/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import url, patterns 
 
 urlpatterns = patterns('mce_pygments.views',
     url(r'^$', 'pygments', name='pygments'),
--- src/django_mce_pygments/views.py
+++ src/django_mce_pygments/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.template import RequestContext
-from django.shortcuts import render_to_response, get_object_or_404
-from django.utils.safestring import mark_safe
+from django.shortcuts import render_to_response
 
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
@@ -11,8 +10,9 @@ def pygments(request, template_name="pygments/pygments.html"):
     if request.method == "POST":
         lang = request.POST.get('lang', 'python')
         code = request.POST.get('code', '')
-        code = highlight(code, get_lexer_by_name(lang),HtmlFormatter(cssclass="pygments"))
+        code = highlight(code, get_lexer_by_name(lang),
+                                       HtmlFormatter(cssclass="pygments"))
         return HttpResponse(code)
     else:
-        return render_to_response(template_name, {}
-        , context_instance=RequestContext(request))
+        return render_to_response(template_name, {},
+                                   context_instance=RequestContext(request))
