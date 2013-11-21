a11e2639e15e5ee81d009ddf8712329022a049b2
Sean O'Donnell <sean@odonnell.nu>
2013-11-21 03:17:03 +0000

    fixing module path

--- django_mce_pygments/urls.py
+++ django_mce_pygments/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import url, patterns 
 
-urlpatterns = patterns('mce_pygments.views',
+urlpatterns = patterns('django_mce_pygments.views',
     url(r'^$', 'pygments', name='pygments'),
 )
