b6562f6c0a826b64844afc7aad4a30f7b5ce2ac9
Sean O'Donnell <sean@odonnell.nu>
2013-11-21 01:51:06 +0000

    working egg

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
+import os
+import sys
+
 from setuptools import setup, find_packages
 
+version = '0.1'
+
 setup(name='django_mce_pygments',
-	packages=find_packages(),
+    version=version,
+    packages=find_packages(exclude=['ez_setup']),
+    description='',
+    long_description='',
+    include_package_data=True,
+    zip_safe=False,
 )
