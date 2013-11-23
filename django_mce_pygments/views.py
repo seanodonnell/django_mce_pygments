b483cbaaa0c86d0d7e8cc9e48ce0119b49391e91
Sean O'Donnell <sean@odonnell.nu>
2013-11-23 02:30:59 +0000

    rendering right, but tinymce is stripping styles

--- django_mce_pygments/views.py
+++ django_mce_pygments/views.py
@@ -11,7 +11,10 @@ def pygments(request, template_name="pygments/pygments.html"):
         lang = request.POST.get('lang', 'python')
         code = request.POST.get('code', '')
         code = highlight(code, get_lexer_by_name(lang),
-                                       HtmlFormatter(cssclass="pygments"))
+                            HtmlFormatter(
+                                cssclass="pygments pygments_%s" % lang,
+                                full=False, style="friendly", noclasses=True),
+                            )
         return HttpResponse(code)
     else:
         return render_to_response(template_name, {},
