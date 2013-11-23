675a7be94ce1e5f3bf470de3fc6420c90f8b8074
Sean O'Donnell <sean@odonnell.nu>
2013-11-23 02:13:26 +0000

    changing renderer url

--- django_mce_pygments/urls.py
+++ django_mce_pygments/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import url, patterns 
 
 urlpatterns = patterns('django_mce_pygments.views',
-    url(r'^$', 'pygments', name='pygments'),
+    url(r'^renderer/$', 'pygments', name='pygments'),
 )
