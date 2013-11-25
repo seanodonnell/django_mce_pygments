6b33ceb875f29140d63d15ece1c45c3abc6c4c06
Sean O'Donnell <sean@odonnell.nu>
2013-11-25 00:50:11 +0000

    download links now available

--- README.txt
+++ README.txt
@@ -4,3 +4,8 @@ some settings you can define
 
 MCE_DEFAULT_LEXERS - languages in this list will be the first in the dropdown 
 MCE_DEFAULT_STYLE - the default style to have selected in the pygments dialog
+SOURCE_CODE_FILE_DIR - the location to store raw source code used in blog posts
+                       default is static/source, make sure it exists and is
+                       writable
+
+The template pygments/source_link.html can be overridden or customized to change how the download link for source code is presented.
--- django_mce_pygments/views.py
+++ django_mce_pygments/views.py
@@ -1,5 +1,8 @@
+import os
+
 from django.http import HttpResponse
-from django.template import RequestContext
+from django.template import RequestContext, Context
+from django.template.loader import get_template
 from django.shortcuts import render_to_response
 from django.conf import settings
 
@@ -17,12 +20,16 @@ def pygments(request, template_name="pygments/pygments.html"):
         linenos = request.POST.get('linenos', False)
         linenostart = int(request.POST.get('linenostart', 1))
         highlight_lines = request.POST.get('highlight', '').split(',')
+        srcattach = request.POST.get('srcattach', False)
+        if srcattach == "false":
+            srcattach = False
+        src_attach_name = request.POST.get('srcattachname', '')
         if linenos == 'true':
             linenos = 'inline'
         else:
             linenos = False
              
-        code = highlight(code, get_lexer_by_name(lang),
+        highlighted_code = highlight(code, get_lexer_by_name(lang),
                             HtmlFormatter(
                                 cssclass="pygments pygments_%s" % lang,
                                 full=False, style=style, noclasses=True,
@@ -30,7 +37,20 @@ def pygments(request, template_name="pygments/pygments.html"):
 				linenostart=linenostart,
                                 hl_lines=highlight_lines),
                             )
-        return HttpResponse(code)
+        if srcattach:
+            source_dir = os.path.join(settings.STATIC_ROOT,
+                               getattr(settings,'SOURCE_CODE_FILE_DIR',
+                               'static/source/'))
+            file_name, source_file = make_unique_file(source_dir,
+                                                      src_attach_name)
+            source_file.write(code)
+            source_file.close() 
+            template = get_template('pygments/source_link.html')
+            context = Context({"file_name": file_name})
+            link_html = template.render(context)
+            highlighted_code += link_html
+
+        return HttpResponse(highlighted_code)
     else:
         styles = list(get_all_styles())
         styles.sort()
@@ -54,3 +74,14 @@ def pygments(request, template_name="pygments/pygments.html"):
         context = {"styles": styles, "langs": langs,}
         return render_to_response(template_name, context,
                                    context_instance=RequestContext(request))
+
+def make_unique_file(source_dir, src_attach_name):
+	current_path = os.path.join(source_dir, src_attach_name)
+        attempt_count = 1
+        modified_name = src_attach_name
+        while os.path.exists(current_path):
+            modified_name = ('_%s' % attempt_count).join(
+                                 os.path.splitext(src_attach_name))
+            attempt_count += 1
+            current_path = os.path.join(source_dir, modified_name)
+        return modified_name, open(current_path, "w")
