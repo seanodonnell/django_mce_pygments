0f63686266ef099ce93501c609c81f14ec7ee03f
Sean O'Donnell <sean@odonnell.nu>
2013-12-01 20:34:02 +0000

    setup.py improvements

--- setup.py
+++ setup.py
@@ -6,10 +6,23 @@ from setuptools import setup, find_packages
 version = '0.1'
 
 setup(name='django_mce_pygments',
+    description='Django app that provides a pygments plugin for tinymce and backend functionality. Particularly useful for Mezzanine',
+    long_description='Django app that provides a pygments plugin for tinymce and backend functionality. Particularly useful for Mezzanine',
+    author = "Sean O\'Donnell",
+    author_email = "sean@odonnell.nu",
+    url = "https://github.com/seanodonnell/django_mce_pygments",
+    download_url = "https://github.com/seanodonnell/django_mce_pygments/archive/master.zip",
+    keywords = ["django", "tinymce", "pygments", "mezzanine"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Environment :: Plugins",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+    ],
     version=version,
     packages=find_packages(exclude=['ez_setup']),
-    description='',
-    long_description='',
     include_package_data=True,
     zip_safe=False,
 )
