ba5ca4f7e128adc30f52fed51189e22e6a107293
Sean O'Donnell <sean@odonnell.nu>
2016-09-19 22:59:42 +0100

    adding pygments to requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 from setuptools import setup, find_packages
 
-version = '0.1'
+version = '0.1.1'
 
 setup(name='django_mce_pygments',
     description='Django app that provides a pygments plugin for tinymce and backend functionality. Particularly useful for Mezzanine',
@@ -25,4 +25,8 @@ setup(name='django_mce_pygments',
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
     zip_safe=False,
+    install_requires=[
+        'pygments'
+    ],
+
 )
